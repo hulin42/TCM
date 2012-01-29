4347afb80ade3b5bfaf3e33141dd7ff77d6b2486
Todd Hulin <todd@woodenteeth.com>
2012-01-29 17:48:31 -0500

    Subnav in better shape.
    Got the sub-nav to work (different chunk of code from http://www.svennerberg.com/2009/02/creating-a-submenu-in-wordpress/), but still need to figure out how to make the parent page display uniquely. Something needs to happen in the if statement when it sees a "parent" page.

--- sidebar.php
+++ sidebar.php
@@ -1,22 +1,35 @@
 <div id="primary-widget-area">
 
 	<div id="subnav">
-		<span class="title"><?php the_title();?></span>
-
-		<?php if(!$post->post_parent){ 
-			$children = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0');
-		
-			}else{
-		
-			if($post->ancestors)
-				{$ancestors = end($post->ancestors); $children = wp_list_pages('title_li=&child_of='.$ancestors.'&echo=0');}
-			}
-
-		if ($children) {?>
-		
-		<ul> <?php echo $children; ?></ul>
-		<?php } ?>		
-				
+	
+	<?php
+	$has_subpages = false;
+	// Check to see if the current page has any subpages
+	$children = wp_list_pages('&child_of='.$post->ID.'&echo=0');
+	if($children) {
+	    $has_subpages = true;
+	}
+	// Reseting $children
+	$children = "";
+	
+	// Fetching the right thing depending on if we're on a subpage or on a parent page (that has subpages)
+	if(is_page() && $post->post_parent) {
+	    // This is a subpage
+	    $children = wp_list_pages("title_li=&include=".$post->post_parent ."&echo=0");
+	    $children .= wp_list_pages("title_li=&child_of=".$post->post_parent ."&echo=0");
+	} else if($has_subpages) {
+	    // This is a parent page that have subpages
+	    $children = wp_list_pages("title_li=&include=".$post->ID ."&echo=0");
+	    $children .= wp_list_pages("title_li=&child_of=".$post->ID ."&echo=0");
+	}
+	?>
+	<?php // Check to see if we have anything to output ?>
+	<?php if ($children) { ?>
+	<ul class="submenu">
+	    <?php echo $children; ?>
+	</ul>
+	<?php } ?>
+					
 	</div>
 
 </div>
